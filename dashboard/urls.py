d4463c23bdd137a24214a49205d0f519f45dcdf3
ADNAN-an <adnan.anjar2002@gmail.com>
2024-01-14 12:58:45 +0100

    election view fixed

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,6 +5,6 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('', views.index, name='dashboard'),
     path('settings/', views.settings, name='settings'),
-    path('elections/', views.elections, name='elections'),
+    path('elections/', views.election_view, name='elections'),
     path('details/', views.details, name='details'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -8,8 +8,15 @@ def index(request):
 def settings(request):
     return render(request, 'settings_page.html')
 
-def elections(request):
-    return render(request, 'elections_page.html')
+from django.shortcuts import render
+from iElect.models import Election, Candidate
+
+def election_view(request):
+ elections = Election.objects.all()
+ for election in elections:
+   election.candidates = Candidate.objects.filter(election=election)
+ print(f"Number of elections: {len(elections)}")
+ return render(request, 'elections_page.html', {'elections': elections})
 
 def details(request):
     return render(request, 'details_page.html')
