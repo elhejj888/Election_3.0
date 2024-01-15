99291abfc88f5b80bb5fe7bdb677f0e286485b76
ADNAN-an <adnan.anjar2002@gmail.com>
2024-01-15 18:05:26 +0100

    clean

--- core/settings.py
+++ core/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'iElect',
+        'USER': 'postgres',
+        'PASSWORD': 'iElect',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
